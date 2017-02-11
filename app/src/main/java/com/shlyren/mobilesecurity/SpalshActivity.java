6f62b656ed52bd0320dd93b92289303dc0ca7fbb
任玉祥 <mail@yuxiang.ren>
2017-02-11 15:35:26 +0800

    init project

---
+++ app/src/main/java/com/shlyren/mobilesecurity/SpalshActivity.java
@@ -0,0 +1,14 @@
+package com.shlyren.mobilesecurity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpalshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+
+    }
+}
